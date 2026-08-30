df45d836e86997d1811c60f45fdd188b4415181f
spurthymallesh <spurthymallesh1@gmail.com>
2026-08-30 14:32:30 +0000

    Implement shell prompt and input loop

--- src/main.c
+++ src/main.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+
+#define INPUT_SIZE 1024
 
 int main(void)
 {
+    char input[INPUT_SIZE];
+
     while (1)
     {
         printf("msh> ");
         fflush(stdout);
-
-        char input[1024];
 
         if (fgets(input, sizeof(input), stdin) == NULL)
         {
             printf("\n");
             break;
         }
+
+        input[strcspn(input, "\n")] = '\0';
+
+        if (strlen(input) == 0)
+        {
+            continue;
+        }
+
+        printf("Command entered: %s\n", input);
     }
 
     return 0;
