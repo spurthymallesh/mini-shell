a9aec55ef4140b69607be5f9b994e5bda6ce0bf7
spurthymallesh <spurthymallesh1@gmail.com>
2026-08-30 14:40:40 +0000

    Implement external command execution

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define INPUT_SIZE 1024
+#define MAX_ARGS 64
 
 int main(void)
 {
     char input[INPUT_SIZE];
+    char *args[MAX_ARGS];
 
     while (1)
     {
@@ -25,7 +31,39 @@ int main(void)
             continue;
         }
 
-        printf("Command entered: %s\n", input);
+        int argc = 0;
+        char *token = strtok(input, " ");
+
+        while (token != NULL && argc < MAX_ARGS - 1)
+        {
+            args[argc++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        args[argc] = NULL;
+
+        pid_t pid = fork();
+
+        if (pid < 0)
+        {
+            perror("fork");
+            continue;
+        }
+
+        if (pid == 0)
+        {
+            execvp(args[0], args);
+
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        }
+
+        int status;
+
+        if (waitpid(pid, &status, 0) < 0)
+        {
+            perror("waitpid");
+        }
     }
 
     return 0;
